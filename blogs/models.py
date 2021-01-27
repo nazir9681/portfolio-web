ec67dd42200c9f4088e136935a0c16cb085f1e5c
Nazir <hussainnazir80@gmail.com>
2021-01-27 23:30:42 +0530

    create model of blog and add admin

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blogs(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to="image/")
+    def __str__(self):
+        return self.title
